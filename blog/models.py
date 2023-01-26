0f06ebfd01ac897a8795eff6b685d92feefc0871
undefeel <amir_inalov@inbox.ru>
2023-01-26 21:19:22 +0300

    add comment and blog models

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+
+from core.models import BaseModel
+
+
+class BlogModel(BaseModel):
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    
+    def __str__(self) -> str:
+        return self.title
+
+
+class CommentModel(BaseModel):
+    name = models.CharField(max_length=100)
+    comment = models.TextField()
+    blog =  models.ForeignKey('BlogModel', related_name='blog', on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.name
