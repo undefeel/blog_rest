7aab616e02877bda103d64ac34f1b7c341674ba8
undefeel <amir_inalov@inbox.ru>
2023-02-03 02:51:47 +0300

    link user with comment

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 from core.models import BaseModel
 
@@ -7,7 +8,7 @@ from core.models import BaseModel
 class BlogModel(BaseModel):
     title = models.CharField(max_length=150)
     text = models.TextField()
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self) -> str:
         return self.title
@@ -17,6 +18,7 @@ class CommentModel(BaseModel):
     name = models.CharField(max_length=100)
     comment = models.TextField()
     blog =  models.ForeignKey('BlogModel', related_name='blog', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self) -> str:
         return self.name
